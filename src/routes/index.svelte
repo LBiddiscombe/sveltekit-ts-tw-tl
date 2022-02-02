0330d3e4eac2e0699aa5acab921450ebda0d4631
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2022-02-02 20:50:39 +0000

    added tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container max-w-3xl pt-4 mx-auto">
+	<h1 class="text-xl text-gray-900">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
